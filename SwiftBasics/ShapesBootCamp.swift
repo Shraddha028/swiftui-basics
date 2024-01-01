489fdc7718b63c71965c3cf616171bc1d447e2d9
Shraddha <shraddhasutar28@gmail.com>
2024-01-01 17:37:36 +0530

    Shape basics: circle. rounded rectangle , ellipse

---
+++ SwiftBasics/ShapesBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  ShapesBootCamp.swift
+//  SwiftBasics
+//
+//  Created by Shraddha Sutar on 01/01/24.
+//
+
+import SwiftUI
+
+struct ShapesBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapesBootCamp()
+}
